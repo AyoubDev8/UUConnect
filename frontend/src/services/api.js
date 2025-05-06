51fd2bea7b472815faeeb6321cd6c04516cda55b
Ayoub Djellaoui <ayoubdjellaoui@Ayoubs-MacBook-Pro.local>
2025-05-06 05:25:58 +0100

    Updated the api url after successful deployement of the backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'uuconnect-backend.azurewebsites.net/api';
 
 const api = axios.create({
   baseURL: API_URL,
